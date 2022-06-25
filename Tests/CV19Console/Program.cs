0cf9d8aa97ea672fce559c3a9ced5ee6bfc1e3f0
Serdar Ablyametov <useinovserdar23@gmail.com>
2022-06-25 10:51:43 +0500

    Запрос к удаленному сервису

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV19Console
 {
     class Program
     {
+        const string dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
+            var client = new HttpClient();
+
+            var response = client.GetAsync(dataUrl).Result;
+            var cvr_str = response.Content.ReadAsStringAsync().Result;
+
+
             Console.WriteLine("Hello World!");
         }
     }
